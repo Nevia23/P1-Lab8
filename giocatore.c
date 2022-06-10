934427f3c8ea8ed39e6626d5428af5d0ad6f7f70
Nevia23 <81696502+Nevia23@users.noreply.github.com>
2022-06-10 14:56:27 +0200

    Add files via upload

---
+++ giocatore.c
@@ -0,0 +1,18 @@
+#include "giocatore.h"
+#include "board.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void scegli_mossa_utente(int *x, int *y) {
+    printf("Inserisci Mossa: ");
+    scanf("%d %d", x, y);
+}
+
+
+void scegli_mossa_computer(int *x, int *y) {
+    
+    // Inserite qua il vostro codice
+    
+}
+
---
+++ partita.c
@@ -0,0 +1,10 @@
+#include "partita.h"
+#include "board.h"
+#include "giocatore.h"
+#include <stdio.h>
+
+void gioca(char board[], void(*p1)(int *x, int *y), void(*p2)(int *x, int *y)) {
+
+    // Inserite qua il vostro codice
+    
+}
